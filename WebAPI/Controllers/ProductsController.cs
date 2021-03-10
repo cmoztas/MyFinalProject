089d2992adc528a8a3575f6f4272557a7e762d1a
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-03-11 01:36:14 +0300

    Test için yazılan thread.sleep kaldırıldı ve categories controller'ı eklendi

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -21,7 +21,6 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-            Thread.Sleep(5000);
             var result = _productService.GetAll();
             if (result.Success)
             {
